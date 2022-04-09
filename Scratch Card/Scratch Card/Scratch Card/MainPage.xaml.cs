0d75d300d103918d3daa27dc827593f9739ae3bd
Chandan Y S <chandanys18@gmail.com>
2022-04-09 13:58:46 +0530

    added project

---
+++ Scratch Card/Scratch Card/Scratch Card/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using Scratch_Card.Views;
+using System;
+using Xamarin.Forms;
+
+namespace Scratch_Card
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            var control = this;
+            control.Opacity = 1;
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            var control = this;
+            control.Opacity = 0.6;
+
+            _ = Navigation.PushModalAsync(new ScratchView(), false);
+        }
+    }
+}
---
+++ Scratch Card/Scratch Card/Scratch Card/Views/ScratchView.xaml.cs
@@ -0,0 +1,144 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using SkiaSharp;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using TouchTracking;
+using System;
+
+namespace Scratch_Card.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ScratchView : ContentPage
+    {
+        Dictionary<long, SKPath> inProgressPaths = new Dictionary<long, SKPath>();
+        List<SKPath> completedPaths = new List<SKPath>();
+
+        SKBitmap rect = new SKBitmap();
+        SKRect sKRect = new SKRect();
+        SKPaint paint = new SKPaint
+        {
+            Style = SKPaintStyle.Stroke,
+            BlendMode = SKBlendMode.Clear,
+            StrokeCap = SKStrokeCap.Round,
+            StrokeJoin = SKStrokeJoin.Round
+        };
+
+        bool IsScratched = false;
+        long sensitivityCount = 0;
+
+        public ScratchView()
+        {
+            InitializeComponent();
+
+            paint.StrokeWidth = 80;
+
+            ScratchView control = this;
+            control.rect = control.GetBitMap("SC2.png");
+        }
+
+        SKBitmap GetBitMap(string resourceName)
+        {
+            SKBitmap resourceBitmap;
+
+            Assembly assembly = GetType().GetTypeInfo().Assembly;
+            string resourceId = $"{assembly.GetName().Name}.{resourceName}";
+
+            //If the project name contains space, replace with underscore(_)...
+            resourceId = resourceId.Replace(' ', '_');
+
+            using(Stream stream = assembly.GetManifestResourceStream(resourceId))
+            {
+                resourceBitmap = SKBitmap.Decode(stream);
+            }
+
+            return resourceBitmap;
+        }
+
+        private void CanvasView_PaintSurface(object sender, SkiaSharp.Views.Forms.SKPaintSurfaceEventArgs e)
+        {
+            SKCanvas canvas = e.Surface.Canvas;
+            canvas.Clear();
+            canvas.DrawBitmap(rect, e.Info.Rect);
+
+            float area = sKRect.Width * sKRect.Height;
+
+            foreach(SKPath path in completedPaths)
+            {
+                canvas.DrawPath(path, paint);
+                sKRect.Size = new SKSize(100, 100);
+                path.GetBounds(out sKRect);
+            }
+
+            foreach(SKPath path in inProgressPaths.Values)
+            {
+                canvas.DrawPath(path, paint);
+                sKRect.Size = new SKSize(100, 100);
+                path.GetBounds(out sKRect);
+
+                sensitivityCount++;
+
+                if(sensitivityCount >= 120)
+                {
+                    if(!IsScratched)
+                    {
+                        IsScratched = true;
+                        ScratchView control = this;
+                        control.rect = control.GetBitMap("SC4.png");
+                        Random random = new Random();
+                        TxtTest.Text = random.Next(1, 100).ToString();
+                    }
+                }
+            }
+        }
+
+        private void TouchEffect_TouchAction(object sender, TouchTracking.TouchActionEventArgs args)
+        {
+            switch(args.Type)
+            {
+                case TouchActionType.Pressed:
+                    if(!inProgressPaths.ContainsKey(args.Id))
+                    {
+                        SKPath path = new SKPath();
+                        path.MoveTo(ConvertToPixel(args.Location));
+                        inProgressPaths.Add(args.Id, path);
+                        canvasView.InvalidateSurface();
+                    }
+                    break;
+
+                case TouchActionType.Moved:
+                    if(inProgressPaths.ContainsKey(args.Id))
+                    {
+                        SKPath path = inProgressPaths[args.Id];
+                        path.LineTo(ConvertToPixel(args.Location));
+                        canvasView.InvalidateSurface();
+                    }
+                    break;
+
+                case TouchActionType.Released:
+                    if(inProgressPaths.ContainsKey(args.Id))
+                    {
+                        completedPaths.Add(inProgressPaths[args.Id]);
+                        inProgressPaths.Remove(args.Id);
+                        canvasView.InvalidateSurface();
+                    }
+                    break;
+
+                case TouchActionType.Cancelled:
+                    if(inProgressPaths.ContainsKey(args.Id))
+                    {
+                        inProgressPaths.Remove(args.Id);
+                        canvasView.InvalidateSurface();
+                    }
+                    break;
+            }
+        }
+
+        SKPoint ConvertToPixel(TouchTrackingPoint pt)
+        {
+            return new SKPoint((float)(canvasView.CanvasSize.Width * pt.X / canvasView.Width),
+                               (float)(canvasView.CanvasSize.Height * pt.Y / canvasView.Height));
+        }
+    }
+}
